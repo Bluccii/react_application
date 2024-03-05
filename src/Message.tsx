4ef84749416360b54b02ffae5779748a68ea0037
Ivan Wu <ivanwubusiness@gmail.com>
2024-03-05 15:47:58 -0500

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing - First letter of every word is capitalized
+
+function Message() {
+    // JSX: JavaScript XML code
+    const name = 'Ivan';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
