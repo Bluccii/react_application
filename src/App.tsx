defa1462abd80a1cb0b3120bc3b8c55a81396e2c
Ivan Wu <ivanwubusiness@gmail.com>
2024-03-05 19:45:30 -0500

    New Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Toyko", "London", "Paris"];
-
-  const handleSlectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSlectItem}
-      />
+      <Alert />
     </div>
   );
 }
