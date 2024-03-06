83f4bb6e478ba51b4a78c3d002a9b2c1b3d06c15
Ivan Wu <ivanwubusiness@gmail.com>
2024-03-05 19:41:34 -0500

    2nd Commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import ListGroup from "./components/ListGroup";
 function App() {
   let items = ["New York", "San Francisco", "Toyko", "London", "Paris"];
 
+  const handleSlectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSlectItem}
+      />
     </div>
   );
 }
